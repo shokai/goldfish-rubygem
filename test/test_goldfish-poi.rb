0ab2869c150734ac63277c2f25a2070cf70063e5
Sho Hashimoto <hashimoto@shokai.org>
2012-03-08 01:47:30 +0900

    add test

--- test/test_goldfish-poi.rb
+++ test/test_goldfish-poi.rb
@@ -3,11 +3,24 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestGoldfishPoi < Test::Unit::TestCase
 
   def setup
-    @a = :asdf
+    @tag = "test_#{Time.now.to_i}_#{Time.now.usec}"
+    @data = "data_#{Time.now.to_i}_#{Time.now.usec}"
+    @poi = GoldFish::Poi.new('http://ubif.org:8932')
   end
-  
-  def test_truth
-    puts @a
-    assert true
+
+  def test_post_get
+    @poi.post(@tag, @data)
+    @res = @poi.get(@tag)
+    assert true if @res == @data
   end
+
+  def test_commet
+    Thread.new do
+      @res = @poi.get(@tag)
+    end
+    sleep 3
+    @poi.post(@tag, @data)
+    assert true if @res == @data
+  end
+
 end
